aa852bd21700653a16323b7b11a242c3241f62ea
LuizLackeski <luiz.lackeski@cobli.co>
2026-04-26 15:12:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -86,4 +86,7 @@ app.post('/api/distancia', async (req, res) => {
 
 // --- AJUSTE 2: Porta Dinâmica ---
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}!`));
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}!`);
+});
